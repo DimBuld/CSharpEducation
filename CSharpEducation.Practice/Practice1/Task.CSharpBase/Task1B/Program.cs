2bb3cc82629f45370335c5acf422852ae19b5e6f
DimBuld <dbmersin@gmail.com>
2026-05-06 16:59:30 +0400

    Правильные файлы

---
+++ CSharpEducation.Practice/Practice1/Task.CSharpBase/Task1B/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task1B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Pratice3/Task04/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace Task04
+{
+    internal class Book
+    {
+        public string nazvanie;
+        public string avtor;
+        public Book(string naz, string av)
+        {
+            nazvanie = naz;
+            avtor = av;
+        }
+        public Book()
+        {
+            nazvanie = "книга";
+            avtor = "Я";
+        }
+        
+    }
+}
